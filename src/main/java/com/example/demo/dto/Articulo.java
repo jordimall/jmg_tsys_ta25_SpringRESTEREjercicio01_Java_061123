dbe141c3b57b9b6b614df65a022dbbb609ce5db4
jordimall <jordi.mallafre@hotmail.com>
2023-11-06 19:54:30 +0100

    Creación de los DTO de Fabricante y Articulo

---
+++ src/main/java/com/example/demo/dto/Articulo.java
@@ -0,0 +1,119 @@
+/**
+ * 
+ */
+package com.example.demo.dto;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+/**
+ * 
+ */
+@Entity
+@Table(name = "articulos")
+public class Articulo {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // busca ultimo valor e incrementa desde id final de db
+	@Column(name = "Codigo")
+	private int codigo;
+
+	@Column(name = "Nombre")
+	private String nombre;
+
+	@Column(name = "Precio")
+	private int precio;
+
+	@ManyToOne
+	@JoinColumn(name = "Fabricante")
+	@Column(name = "Fabricante")
+	private Fabricante fabricante;
+
+	/**
+	 * 
+	 */
+	public Articulo() {
+	}
+
+	/**
+	 * @param codigo
+	 * @param nombre
+	 * @param precio
+	 * @param fabricante
+	 */
+	public Articulo(int codigo, String nombre, int precio, Fabricante fabricante) {
+		this.codigo = codigo;
+		this.nombre = nombre;
+		this.precio = precio;
+		this.fabricante = fabricante;
+	}
+
+	/**
+	 * @return the codigo
+	 */
+	public int getCodigo() {
+		return codigo;
+	}
+
+	/**
+	 * @return the nombre
+	 */
+	public String getNombre() {
+		return nombre;
+	}
+
+	/**
+	 * @return the precio
+	 */
+	public int getPrecio() {
+		return precio;
+	}
+
+	/**
+	 * @return the fabricante
+	 */
+	public Fabricante getFabricante() {
+		return fabricante;
+	}
+
+	/**
+	 * @param codigo the codigo to set
+	 */
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+
+	/**
+	 * @param nombre the nombre to set
+	 */
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	/**
+	 * @param precio the precio to set
+	 */
+	public void setPrecio(int precio) {
+		this.precio = precio;
+	}
+
+	/**
+	 * @param fabricante the fabricante to set
+	 */
+	public void setFabricante(Fabricante fabricante) {
+		this.fabricante = fabricante;
+	}
+
+	@Override
+	public String toString() {
+		return "Articulo [codigo=" + codigo + ", nombre=" + nombre + ", precio=" + precio + ", fabricante=" + fabricante
+				+ "]";
+	}
+
+}
